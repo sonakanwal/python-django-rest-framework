129aad9ee0aed6b0d1a10efb3cd116cc80e07b23
sonakanwal <49017211+sonakanwal@users.noreply.github.com>
2019-08-26 12:22:10 +0500

    Add files via upload

--- author/urls.py
+++ author/urls.py
@@ -6,6 +6,4 @@ app_name = "authors"
 # app_name will help us do a reverse look-up latter.
 urlpatterns = [
     path('authors/', AuthorView.as_view()),
-    # path('create_article/', ArticleCreateView.as_view()),
-    # path('articles/<int:pk>/', ArticleDetail.as_view()),
 ]
